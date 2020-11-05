ea42a74c770583142f770c37c841b11d458967d3
Emerald Elnas <emerald@hyperstacksinc.com>
2020-11-06 03:02:40 +0800

    add PreloadAllModules strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { MinistryBazaarComponent } from './pages/ministry-bazaar/ministry-bazaar.component';
 
 import { ExaltComponent } from './pages/ministries/exalt/exalt.component';
@@ -33,7 +33,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, scrollPositionRestoration: 'top' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
